225342ac91ff303372fff7f8cf6dec589ffaec8d
ADAM GRUSHCOW <adamgrushcow@gmail.com>
2016-12-07 11:22:29 -0800

    Made mobile optimized. Added additional API call and final page logic

--- js/main.js
+++ js/main.js
@@ -2,106 +2,248 @@ var currentQuestion;
 var score;
 var incorrect;
 var correct;
+var questions = [];
 
-var question = [
-	{ text:"What are the five countries with the largest population?",
-		options:[
-		{ optionText:"China", optionValue: 30, answered:0 },
-		{ optionText:"India", optionValue: 22, answered:0  },
-		{ optionText:"United States", optionValue: 18, answered:0  },
-		{ optionText:"Indonesia", optionValue: 15, answered:0  },
-		{ optionText:"Brazil", optionValue: 10, answered:0  },
-		{ optionText:"Russia", optionValue: 5, answered:0  }
-	]},
-/*
-	{ text:"Question 2",
-		options:[
-		{ optionText:"China", optionValue: 30 },
-		{ optionText:"India", optionValue: 22 },
-		{ optionText:"United States", optionValue: 18 },
-		{ optionText:"Indonesia", optionValue: 15 },
-		{ optionText:"Brazil", optionValue: 10 },
-		{ optionText:"Russia", optionValue: 5 }
-	]},
+      $.ajax({
+         url: "http://api.aerisapi.com/countries/search?query=pop:0&sort=pop:-1&limit=6&client_id=zCqKvajWEA7fuUnM810gh&client_secret=ArhTjfFVIfdZyZqWlJZ4j4L3NwtKkp9J3VavqTIr",
+         dataType: "jsonp",
+         success: function(json) {
+         	console.log(json.response);
+            if (json.success == true) {
+               var ob = json.response;     
+               var countryArr = [];
+               var val = 30;
+               var totalPopulation = 0;
 
-	{ text:"Question 3",
-		options:[
-		{ optionText:"China", optionValue: 30 },
-		{ optionText:"India", optionValue: 22 },
-		{ optionText:"United States", optionValue: 18 },
-		{ optionText:"Indonesia", optionValue: 15 },
-		{ optionText:"Brazil", optionValue: 10 },
-		{ optionText:"Russia", optionValue: 5 }
-	]},
+               for(i=0; i < ob.length; i++) {
+               	var option = {}
+               	option.optionText = ob[i].place.name;
+               	option.optionValue = ob[i].profile.pop;
+               	option.answered = 0;
+                  countryArr.push(option);
+                totalPopulation = totalPopulation + ob[i].profile.pop;
+               }  
 
-	{ text:"Question 4",
-		options:[
-		{ optionText:"China", optionValue: 30 },
-		{ optionText:"India", optionValue: 22 },
-		{ optionText:"United States", optionValue: 18 },
-		{ optionText:"Indonesia", optionValue: 15 },
-		{ optionText:"Brazil", optionValue: 10 },
-		{ optionText:"Russia", optionValue: 5 }
-	]},
-*/
-];
+		      for (i = 0; i < countryArr.length; i++) {
+		      	var percent = (countryArr[i].optionValue/totalPopulation) * 100;
+		      	countryArr[i].optionValue =  Math.floor(percent);
+		      }              
+
+            var question1 = {};
+            question1.questionText = "What are the countries with the largest population?";
+            question1.options = countryArr;
+
+            questions.push(question1);
+
+            askQuestion();
+            }
+            else {
+               alert('An error occurred: ' + json.error.description);
+            }
+         }
+      });
+
+      $.ajax({
+         url: "http://api.aerisapi.com/countries/search?query=area:7000000:20000000&limit=6&client_id=zCqKvajWEA7fuUnM810gh&client_secret=ArhTjfFVIfdZyZqWlJZ4j4L3NwtKkp9J3VavqTIr",
+         dataType: "jsonp",     	
+         success: function(json) {
+         	console.log(json.response);
+            if (json.success == true) {
+               var ob = json.response;     
+               var countryArr = [];
+               var val = 30;
+               var totalArea = 0;
+
+               ob.sort(function(a, b){
+					return b.profile.areaKM - a.profile.areaKM;
+				});
+
+               for(i=0; i < ob.length; i++) {
+               	var option = {}
+               	option.optionText = ob[i].place.name;
+               	option.optionValue = ob[i].profile.areaKM;
+               	option.answered = 0;
+                  countryArr.push(option);
+                totalArea = totalArea + ob[i].profile.areaKM;
+               }  
+
+		      for (i = 0; i < countryArr.length; i++) {
+		      	var percent = (countryArr[i].optionValue/totalArea) * 100;
+		      	countryArr[i].optionValue =  Math.floor(percent);
+		      }              
+
+            var question2 = {};
+            question2.questionText = "What are the countries with the largest area?";
+            question2.options = countryArr;
+
+            questions.push(question2);
+
+            askQuestion();
+            }
+            else {
+               alert('An error occurred: ' + json.error.description);
+            }
+         }
+      });
+
+/*      $.ajax({
+      	url: "https://api.aerisapi.com/records/search?filter=maxt?query=year:2015&&sort=val&limit=6&client_id=zCqKvajWEA7fuUnM810gh&client_secret=ArhTjfFVIfdZyZqWlJZ4j4L3NwtKkp9J3VavqTIr",
+      	//url: "https://api.aerisapi.com/earthquakes/search?query=mag&sort=mag:-1&limit=6&client_id=zCqKvajWEA7fuUnM810gh&client_secret=ArhTjfFVIfdZyZqWlJZ4j4L3NwtKkp9J3VavqTIr",
+         dataType: "jsonp",     	
+         success: function(json) {
+         	console.log(json.response);
+            if (json.success == true) {
+               var ob = json.response;     
+               var countryArr = [];
+               var val = 30;
+               var totalMag = 0;
+
+               ob.sort(function(a, b){
+					return b.report.mag - a.report.mag;
+				});
+
+               for(i=0; i < ob.length; i++) {
+               	var option = {}
+               	option.optionText = ob[i].place.country;
+               	option.optionValue = ob[i].report.mag;
+               	option.answered = 0;
+                  countryArr.push(option);
+                totalMag = totalMag + ob[i].report.mag;
+               }  
+
+
+		      for (i = 0; i < countryArr.length; i++) {
+		      	var percent = (countryArr[i].optionValue/totalMag) * 100;
+		      	countryArr[i].optionValue =  Math.floor(percent);
+		      }              
+
+            var question3 = {};
+            question3.questionText = "What countries have the largest earthquakes occured in?";
+            question3.options = countryArr;
+
+            questions.push(question3);
+
+            askQuestion();
+            }
+            else {
+               alert('An error occurred: ' + json.error.description);
+            }
+         }
+      });*/
+
+      $.ajax({
+      	url: "https://api.aerisapi.com/earthquakes/search?filter=light&sort=mag-1&limit=6&client_id=zCqKvajWEA7fuUnM810gh&client_secret=ArhTjfFVIfdZyZqWlJZ4j4L3NwtKkp9J3VavqTIr",
+      	//url: "https://api.aerisapi.com/earthquakes/search?query=mag&sort=mag:-1&limit=6&client_id=zCqKvajWEA7fuUnM810gh&client_secret=ArhTjfFVIfdZyZqWlJZ4j4L3NwtKkp9J3VavqTIr",
+         dataType: "jsonp",     	
+         success: function(json) {
+         	console.log(json.response);
+            if (json.success == true) {
+               var ob = json.response;     
+               var countryArr = [];
+               var val = 30;
+               var totalMag = 0;
+
+               ob.sort(function(a, b){
+					return b.report.mag - a.report.mag;
+				});
+
+               for(i=0; i < ob.length; i++) {
+               	var option = {}
+               	option.optionText = ob[i].place.country;
+               	option.optionValue = ob[i].report.mag;
+               	option.answered = 0;
+                  countryArr.push(option);
+                totalMag = totalMag + ob[i].report.mag;
+               }  
+
+
+		      for (i = 0; i < countryArr.length; i++) {
+		      	var percent = (countryArr[i].optionValue/totalMag) * 100;
+		      	countryArr[i].optionValue =  Math.floor(percent);
+		      }              
+
+            var question3 = {};
+            question3.questionText = "What countries have the largest earthquakes occured in?";
+            question3.options = countryArr;
+
+            questions.push(question3);
+
+            askQuestion();
+            }
+            else {
+               alert('An error occurred: ' + json.error.description);
+            }
+         }
+      });
 
 var finalRound = [
   {
-    text:"What is the..",
+    text:"What capital city has the largest population?",
     options: [
-      { optionText:"A", optionValue:20 },
-      { optionText:"B", optionValue:30 }
+      { optionText:"Beijing", optionValue:30, answered:0},
+      { optionText:"New Dehli", optionValue:25, answered:0},
+      { optionText:"Tokyo", optionValue:20, answered:0},
+      { optionText:"Manila", optionValue:10, answered:0},
+      { optionText:"Moscow", optionValue:5, answered:0}
     ]
   },
-
   {
-    text:"What is the..",
+    text:"What are the oldest continuously inhabited cities?",
     options: [
-      { optionText:"A", optionValue:20 },
-      { optionText:"B", optionValue:30 }
+      { optionText:"Luxor", optionValue:30, answered:0},
+      { optionText:"Carthage", optionValue:25, answered:0},
+      { optionText:"Tripoli", optionValue:20, answered:0},
+      { optionText:"Benghazi", optionValue:10, answered:0},
+      { optionText:"Axum", optionValue:5, answered:0}
     ]
   },
-
   {
-    text:"What is the..",
+    text:"What are the most populous cities in the US?",
     options: [
-      { optionText:"A", optionValue:20 },
-      { optionText:"B", optionValue:30 }
+      { optionText:"New York City", optionValue:30, answered:0},
+      { optionText:"Los Angeles", optionValue:25, answered:0},
+      { optionText:"Chicago", optionValue:20, answered:0},
+      { optionText:"Houston", optionValue:10, answered:0},
+      { optionText:"Philadelphia", optionValue:5, answered:0}
     ]
   },
-
   {
-    text:"What is the..",
+    text:"What countries are the all-time highest temperatures recorded?",
     options: [
-      { optionText:"A", optionValue:20 },
-      { optionText:"B", optionValue:30 }
+      { optionText:"United States", optionValue:30, answered:0},
+      { optionText:"Tunisia", optionValue:25, answered:0},
+      { optionText:"Israel", optionValue:20, answered:0},
+      { optionText:"Kuwait", optionValue:10, answered:0},
+      { optionText:"Iraq", optionValue:5, answered:0}
     ]
   },
-
   {
-    text:"What is the..",
+    text:"What countries have the lowest recorded temperatures all-time?",
     options: [
-      { optionText:"A", optionValue:20 },
-      { optionText:"B", optionValue:30 }
+      { optionText:"Antarctica", optionValue:30, answered:0},
+      { optionText:"Russia", optionValue:25, answered:0},
+      { optionText:"Greenland", optionValue:20, answered:0},
+      { optionText:"Canada", optionValue:10, answered:0},
+      { optionText:"United States", optionValue:5, answered:0}
     ]
-  },
+  }
 ];
 
-$(document).ready(function (){
+$(document).ready(function () {
 	newGame();
-	askQuestion();
+	//askQuestion();
 
 	$('.question-response').submit(function (event) {
 		var userInput = $('.user-input').val().toLowerCase();
-		var optionsArr = question[currentQuestion].options;
+		var optionsArr = questions[currentQuestion].options;
 		event.preventDefault();
 
 		if ((correct <= 6) && (incorrect <= 3)) {
 
 			if (correct == 5) {
 				match();
-				$('.btn-container').addClass('next-btn');
+				$('.next').show();
+				$('.next-btn').show();
 				$('.question').hide();
 				$('.user-input').hide();
 
@@ -110,9 +252,9 @@ $(document).ready(function (){
 
 			} else {
 				noMatch();
-
 			}
-			$('.user-input').val('');
+
+				$('.user-input').val('');
 
 		} 
 	});
@@ -122,35 +264,22 @@ $(document).ready(function (){
     });
 
 	$('.final-question').keyup(function(e){
-		if (e.which == 13) {
-		    var qnum = $(this).data("questionNumber");
-		    var currentQuestion = finalRound[qnum];
-		    var answer = search($(this).val().toLowerCase(), currentQuestion.options);
+		var qnum = $(this).data("questionNumber");
+	    var currentQuestion = finalRound[qnum];
+	    var answer = search($(this).val().toLowerCase(), currentQuestion.options);
+	    var answered = 0;
 
-		    if(answer > -1){
-		    	$(this).siblings('.q-score').html(currentQuestion.options[answer].optionValue);
-		      //display currentQuestion.options[answer];
-		    }else{
-		    	$(this).siblings('.q-score').html("0");
-		      //display 0
-		    }
-		}
-
-      $.ajax({
-         url: "http://api.aerisapi.com/countries/search?query=pop:0&sort=pop:-1&limit=6&client_id=zCqKvajWEA7fuUnM810gh&client_secret=ArhTjfFVIfdZyZqWlJZ4j4L3NwtKkp9J3VavqTIr",
-         dataType: "jsonp",
-         success: function(json) {
-            if (json.success == true) {
-               var ob = json.response;
-               
-               $('#js').html(ob[0].place.name.toLowerCase());
-            }
-            else {
-               alert('An error occurred: ' + json.error.description);
-            }
-         }
-      });
-
+			if (e.which == 13) {
+			    if(answer > -1){
+			    	if (answered == 1) 
+			    		console.log(answered);
+				    	$(this).siblings('.q-score').html(currentQuestion.options[answer].optionValue);
+				    	score = currentQuestion.options[answer].optionValue + score;
+				    	$('#overall-score').html(score);
+			    } else {
+			    	$(this).siblings('.q-score').html("0");
+			    }
+			}
 });
 
 });
@@ -158,7 +287,6 @@ $(document).ready(function (){
 function newGame() {
 	currentQuestion = 0;
 	score = 0;
-
 	$('.next').hide();
 	$('.final').hide();
 }
@@ -167,7 +295,7 @@ function askQuestion() {
 	correct = 0;
 	incorrect = 0;
 
-	var text = question[currentQuestion].text;
+	var text = questions[currentQuestion].questionText;
 	var span = '<span>' + text + '</span>';
 
 	$('.question').html(span);	
@@ -183,7 +311,7 @@ function nextQuestion() {
 
 	currentQuestion++;
 
-	if (currentQuestion < question.length) {
+	if (currentQuestion < questions.length) {
 		askQuestion();
 		$('#option-one').html(optionResetOne);
 		$('#option-two').html(optionResetTwo);
@@ -224,42 +352,47 @@ function search(needle, haystack){
 }
 
 function match() {
-	var optionsArr = question[currentQuestion].options;
+	var optionsArr = questions[currentQuestion].options;
 	var userInput = $('.user-input').val().toLowerCase();
 	var span = $('<div class="correct">' + '<span>' + userInput + '</span>' + '</div>');
-
 	correct++;
-
+		
 	if (optionsArr[0].optionText.toLowerCase() == userInput ) {
 		$('#option-one').html(span);
 		$('#option-one').append($('<div class="option-score">' + '<span>' + optionsArr[0].optionValue + '</span>' + '</div>'));
 			score = optionsArr[0].optionValue + score;
 			$('.score h1').html(score);
+			playCorrect();
 	} else if (optionsArr[1].optionText.toLowerCase() == userInput ) {
 		$('#option-two').html(span);
 		$('#option-two').append($('<div class="option-score">' + '<span>' + optionsArr[1].optionValue + '</span>' + '</div>'));
 			score = optionsArr[1].optionValue + score;
 			$('.score h1').html(score);
+			playCorrect();
 	} else if (optionsArr[2].optionText.toLowerCase() == userInput ) {
 		$('#option-three').html(span);
 		$('#option-three').append($('<div class="option-score">' + '<span>' + optionsArr[2].optionValue + '</span>' + '</div>'));
 			score = optionsArr[2].optionValue + score;
 			$('.score h1').html(score);
+			playCorrect();
 	} else if (optionsArr[3].optionText.toLowerCase() == userInput ) {
 		$('#option-four').html(span);
 		$('#option-four').append($('<div class="option-score">' + '<span>' + optionsArr[3].optionValue + '</span>' + '</div>'));
 			score = optionsArr[3].optionValue + score;
 			$('.score h1').html(score);
+			playCorrect();
 	} else if (optionsArr[4].optionText.toLowerCase() == userInput ) {
 		$('#option-five').html(span);
 		$('#option-five').append($('<div class="option-score">' + '<span>' + optionsArr[4].optionValue + '</span>' + '</div>'));
 			score = optionsArr[4].optionValue + score;
 			$('.score h1').html(score);
+			playCorrect();
 	} else {
 		$('#option-six').html(span);
 		$('#option-six').append($('<div class="option-score">' + '<span>' + optionsArr[5].optionValue + '</span>' + '</div>'));
 			score = optionsArr[5].optionValue + score;
 			$('.score h1').html(score);
+			playCorrect();
 	}
 }
 
@@ -270,16 +403,20 @@ function noMatch() {
 	if (incorrect == 1) {
 		$('.incorrect').fadeIn(2000);
 		$('#incorrect-one').fadeIn(2000);
+		playIncorrect();
 		$('.incorrect').fadeOut(1000);
 		$('#incorrect-one').fadeOut(1000);
 	} else if (incorrect == 2) {
+		playIncorrect();
 		$('.incorrect').fadeIn(2000);
 		$('#incorrect-two').fadeIn(2000);
+		playIncorrect();
 		$('.incorrect').fadeOut(1000);
 		$('#incorrect-two').fadeOut(1000);
 	} else {
 		$('.incorrect').fadeIn(2000);
 		$('#incorrect-three').fadeIn(2000);
+		playIncorrect();
 		promise();
 		$('.question').hide();
 		$('.user-input').hide();
@@ -303,4 +440,22 @@ function finalSurvey() {
             el.placeholder = finalRound[i].text;
         }
     });
+}
+
+function playCorrect() {
+    $('#correct-audio')[0].volume = 0.5;
+    $('#correct-audio')[0].load();
+    $('#correct-audio')[0].play();
+}
+
+function playIncorrect() {
+    $('#incorrect-audio')[0].volume = 0.5;
+    $('#incorrect-audio')[0].load();
+    $('#incorrect-audio')[0].play();	
+}
+
+function playTheme() {
+    $('#theme-audio')[0].volume = 0.5;
+    $('#theme-audio')[0].load();
+    $('#theme-audio')[0].play();	
 }
